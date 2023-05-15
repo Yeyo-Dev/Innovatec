d7cc59f43971f139f4baa16ab611f27a07a6cfb9
Yeyo Hernandez <luisabrahamvargas261003@gmail.com>
2023-05-15 00:53:48 -0600

    Al consulltar los datos sólo muestra permitidos

--- dataRT.php
+++ dataRT.php
@@ -1,9 +1,10 @@
 <?php
 // Conexión a la base de datos
 include("./conexion.php");
+$idsensor = $_POST['idsensor'];
 // Consulta a la base de datos
 //$sql = "SELECT * FROM datoscircuito WHERE fecha = (SELECT MAX(fecha) FROM datoscircuito)";
-$sql = "SELECT * FROM lecturas WHERE fecha = (SELECT MAX(fecha) FROM lecturas)";
+$sql = "SELECT * FROM lecturas WHERE fecha = (SELECT MAX(fecha) FROM lecturas) AND id_Sensor = '$idsensor';";
 $res = $mysqli->query($sql);
 $tot_registros = mysqli_num_rows($res);
 // Crear array con los datos
--- mostrar_registros.php
+++ mostrar_registros.php
@@ -3,7 +3,8 @@
 include("./conexion.php");
 // Consulta a la base de datos
 //$sql = "SELECT * FROM datoscircuito";
-$sql = "SELECT * FROM lecturas";
+$idsensor = $_POST['idsensor'];
+$sql = "SELECT * FROM lecturas WHERE id_Sensor = '$idsensor';";
 $res = $mysqli->query($sql);
 $tot_registros = mysqli_num_rows($res);
 // Crear array con los datos
